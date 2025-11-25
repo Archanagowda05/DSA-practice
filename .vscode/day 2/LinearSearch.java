88db82e0460faaee183ae29db6c44bede0c3d414
Archanagowda05 <archanagowdaa005@gmail.com>
2025-11-25 22:02:23 +0530

    Updated LinearSearch.java

--- .vscode/day 2/LinearSearch.java
+++ .vscode/day 2/LinearSearch.java
@@ -2,18 +2,18 @@ public class LinearSearch {
     public static void main(String[] args) {
         int arr[] = { 10, 42, 23, 14, 51, 64, 79 };
         int key = 5;
-        int flag=-1;
+        int flag = -1;
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == key) {
-                flag=i;
-                 break;
+                flag = i;
+                break;
             }
 
         }
-        if(flag!=-1)
-            System.out.println("The " + key + " is present in " + flag  +"th index");
-        else 
-            System.out.println("key "+key+" not found");
+        if (flag != -1)
+            System.out.println("The " + key + " is present in " + flag + "th index");
+        else
+            System.out.println("key " + key + " not found");
     }
 
 }
