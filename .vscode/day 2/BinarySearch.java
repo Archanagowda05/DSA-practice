f6f7888553737b39f0ec0dfc7e1860f20c3ce4ab
Archanagowda05 <archanagowdaa005@gmail.com>
2025-11-25 21:51:55 +0530

    Binary search and linear search code added

--- .vscode/day 2/BinarySearch.java
+++ .vscode/day 2/BinarySearch.java
@@ -3,7 +3,7 @@ public class BinarySearch {
         int arr[] = { 1, 2, 3, 4, 5, 6, 7 };
         int key = 10;
         int flag = -1;
-        int low = 0, high = arr.length-1;
+        int low = 0, high = arr.length - 1;
         while (low <= high) {
             int mid = (low + high) / 2;
             if (arr[mid] == key) {
@@ -11,15 +11,14 @@ public class BinarySearch {
                 break;
             } else if (key > arr[mid]) {
                 low = mid + 1;
-                }
-             else if (key <= arr[mid]) {
-                high = mid -1;
-                }
-            } 
-        
-            if(flag!=-1)
-                System.out.println("The " + key + " is present in " + flag + "th index");
-            else
-                System.out.println("key " + key + " not found");
+            } else if (key <= arr[mid]) {
+                high = mid - 1;
+            }
         }
+
+        if (flag != -1)
+            System.out.println("The " + key + " is present in " + flag + "th index");
+        else
+            System.out.println("key " + key + " not found");
     }
+}
